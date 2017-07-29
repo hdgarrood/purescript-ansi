12d8c2ad0c71d3e40e840a39289800a870866451
Harry Garrood <harry@garrood.me>
2017-07-29 23:45:29 +0100

    Make `withGraphics` just return a `String`

--- src/Ansi/Output.purs
+++ src/Ansi/Output.purs
@@ -6,29 +6,25 @@ import Prelude
 
 import Ansi.Codes (Color, EscapeCode(..), GraphicsParam(..), RenderingMode(..), escapeCodeToString)
 
--- | Using the given output function, write the given text with the given set
--- | of graphics parameters. For example:
+-- | Wrap the given text in escape codes corresponding to the given parameters.
+-- | For example:
 -- |
 -- | ```purescript
--- | withGraphics Console.log (bold <> underline <> foreground BrightRed) "hello world"
+-- | Console.log $ withGraphics (bold <> underline <> foreground BrightRed) "hello world"
 -- | ```
 -- |
 -- | would print "hello world" to the terminal, bold, underlined, and in bright
 -- | red, and then reset (so that further logging to the console uses the
 -- | normal color and style).
 -- |
--- | This function works by first printing the escape code corresponding to the
--- | supplied graphics parameters, then printing the text, and then printing a
--- | reset escape code.
--- |
--- | The first argument should take a string, and write it to some output stream
--- | such as a terminal. In most cases, you will want to use either
--- | `Console.log` or `Console.error`.
-withGraphics :: forall m. (Monad m) => (String -> m Unit) -> Array GraphicsParam -> String -> m Unit
-withGraphics write params text = do
-  write (escapeCodeToString (Graphics params))
-  write text
-  write (escapeCodeToString (Graphics [Reset]))
+-- | This function simply wraps the given text in an escape code and a reset
+-- | code, so that it is a little more comfortable to use than the functions
+-- | in `Ansi.Codes`.
+withGraphics :: Array GraphicsParam -> String -> String
+withGraphics params text =
+  escapeCodeToString (Graphics params) <>
+  text <>
+  escapeCodeToString (Graphics [Reset])
 
 bold :: Array GraphicsParam
 bold = [PMode Bold]
